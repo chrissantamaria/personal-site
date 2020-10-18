c813b253f1bcbeeba0e424e3a98b409b7f4151b5
Chris Santamaria <chris@santamaria.me>
2020-10-17 21:25:32 -0400

    Adjust landing container sizing

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,32 +1,35 @@
 import styled from '@emotion/styled';
 
 export const LandingContainer = styled.div({
-  height: '100%',
+  height: 'calc(100vh - 2rem)',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  padding: '0 1rem',
+  padding: '0 32px',
   background: '#543f79',
+  color: 'white',
   textAlign: 'center',
   position: 'relative',
 });
 
 export const HeadshotContainer = styled.div({
   width: 400,
-  maxWidth: '100%',
+  maxWidth: '90%',
   marginBottom: '1rem',
 });
 
 export const Title = styled.h1({
   margin: 0,
-  fontSize: '2.7rem',
-  color: 'white',
+  fontSize: '2.6rem',
   marginBottom: '0.5rem',
 });
 
 export const Subtitle = styled.h2({
   margin: 0,
+  fontSize: '1.7rem',
+  fontWeight: 300,
+});
   fontSize: '1.8rem',
   color: 'white',
   fontWeight: 300,
