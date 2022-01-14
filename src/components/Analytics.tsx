446d0e46db79a1dac2edda2994e6660f00140eb3
Chris Santamaria <chris@santamaria.me>
2022-01-13 22:06:15 -0500

    Change analytics script src location

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script';
 
 const SEO: React.FC = () => (
   <Script
-    src={`${process.env.NEXT_PUBLIC_UMAMI_URL}/umami.js`}
+    src={`${process.env.NEXT_PUBLIC_UMAMI_URL}/stats.js`}
     strategy="afterInteractive"
     data-website-id={process.env.NEXT_PUBLIC_UMAMI_SITE_ID}
   />
