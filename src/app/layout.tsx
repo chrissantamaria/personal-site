bf2f4c90236c41ce6b7e25f55d2cdbb07b2538d8
Chris Santamaria <chris@santamaria.me>
2022-12-09 17:03:22 -0500

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Open_Sans } from '@next/font/google';
 
+import { Analytics } from '../components/Analytics';
+
 import '../styles/globals.css';
 
 const font = Open_Sans({ subsets: ['latin'] });
@@ -7,7 +9,10 @@ const font = Open_Sans({ subsets: ['latin'] });
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={font.className}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 };
