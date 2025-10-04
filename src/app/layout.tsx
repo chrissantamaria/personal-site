c0a28925fb6102fb91c3f979e0b66f87937cdf42
Chris Santamaria <chris@santamaria.me>
2025-10-04 13:16:12 -0400

    Remove @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import { Open_Sans } from 'next/font/google';
 
-import { Analytics } from '~/components/Analytics';
-
 import '../styles/globals.css';
 
 const font = Open_Sans({ subsets: ['latin'] });
@@ -9,10 +7,7 @@ const font = Open_Sans({ subsets: ['latin'] });
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={font.className}>
-      <body>
-        {children}
-        <Analytics />
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
