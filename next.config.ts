10c36c6bbecb59152c8ccbdb694c21f75b0637b5
Chris Santamaria <chris@santamaria.me>
2025-10-04 12:56:11 -0400

    Use Cloudflare Images for transformations

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // https://developers.cloudflare.com/images/transform-images/integrate-with-frameworks/#global-loader
+  images: {
+    loader: 'custom',
+    loaderFile: './imageLoader.ts',
+  },
 };
 
 export default nextConfig;
