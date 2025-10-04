00bf187f0fef6c09333a39da79f1e638c42aa86e
Chris Santamaria <chris@santamaria.me>
2025-10-04 12:44:17 -0400

    Use static export wiith _redirects rules

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async redirects() {
-    return [
-      {
-        source: '/resume.pdf',
-        destination:
-          'https://github.com/chrissantamaria/resume/releases/latest/download/resume.pdf',
-        permanent: false,
-      },
-      {
-        source: '/call',
-        destination: 'https://cal.com/chrissantamaria/30min',
-        permanent: false,
-      },
-    ];
-  },
+  output: 'export',
 };
 
 export default nextConfig;
